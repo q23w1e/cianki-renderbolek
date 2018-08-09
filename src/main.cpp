d20472f61d7302aba06bc97fe48f81d106f28ea2
o_patseev <o_patseev@wargaming.net>
2018-08-09 14:35:16 +0300

    let starts from here

--- src/main.cpp
+++ src/main.cpp
@@ -2,15 +2,26 @@
 #include "TGAImage/tgaimage.h"
 
 
+const TGAColor black = TGAColor(0, 0, 0, 255);
+const TGAColor white = TGAColor(255, 255, 255, 255);
+const TGAColor red = TGAColor(255, 0, 0, 255);
+
 struct Color {
-	TGAColor black = TGAColor(0, 0, 0, 1);
-	TGAColor white = TGAColor(1, 1, 1, 1);
-	TGAColor red = TGAColor(1, 1, 1, 1);
+	const TGAColor black = TGAColor(0, 0, 0, 255);
+	const TGAColor red = TGAColor(255, 0, 0, 255);
 };
 
-
 int main(int argc, char *argv[]) {
-	auto image = TGAImage(300, 300, 1);
+	auto image = TGAImage(300, 300, TGAImage::RGB);
+	
+	size_t size = 100;
+	for(size_t y = 0; y < size; y++)
+		for(size_t x = 0; x < size; x++) {
+			image.set(x, y, red);
+		}
+	
+	image.write_tga_file("result.tga");
+	
 	std::cout << "Hello Easy C++ project!" << std::endl;
 	std::cout << image.get_height() << ", " << image.get_width() << std::endl;
 }
